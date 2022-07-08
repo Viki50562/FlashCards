5103b8cfb2229110cf41371b4baeef10e864a0a7
Viktoria <syrcova.viktorija@gmail.com>
2022-07-08 14:24:27 +0300

    First view

--- View.js
+++ View.js
@@ -1,7 +1,36 @@
+const readLine = require('readline-sync');
+
 class View {
-  constructor() {
+  rl; 
+  controller;
+
+  constructor() {  }
+
+  //Тут вывожу просто конкретный вопрос
+  showQuestion(arrQuestion) {
+    arrQuestion.forEach((el) => console.log(el))
+    // const answer = readLine.question(question);
+  }
+
+  //Тут вывожу эмодзи в зависимости от результата проверки ответа из модели
+  showResult(check) {
 
   }
+
+
+  showCategories(categoriasList) {
+    categoriasList.forEach((el, index) => {
+      console.log(`${index + 1}. ${el}`);
+    });
+  }
+
+  getCategory() {
+    const answer = readLine.question('Выберите категорию: ');
+    return answer;
+  }
+
+
+
 }
 
 module.exports = View
